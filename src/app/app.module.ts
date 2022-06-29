6d0467b4acf27c206b3979b1b76f09d4c24d3635
Dheerendra-Shar <dheerendrasharma394@gmail.com>
2022-06-29 12:59:37 +0530

    server side data table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DataTablesModule } from 'angular-datatables';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    DataTablesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/person.ts
@@ -0,0 +1,5 @@
+export class Person {
+    id: any;
+    firstName: any;
+    lastName: any;
+  }
